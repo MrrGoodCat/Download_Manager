9a3708c832e152bb88b7d809e79d8dc27a62f37e
MrrGoodCat <mrrgoodcat@gmail.com>
2016-10-17 16:56:27 +0300

    Добавьте файлы проекта.

---
+++ DownloadManager/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DownloadManager
+{
+    partial class DownloadManagerForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.progressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.lableProgress = new System.Windows.Forms.Label();
+            this.lableDownloadURL = new System.Windows.Forms.Label();
+            this.lableDownloadPath = new System.Windows.Forms.Label();
+            this.textBoxDownloadURL = new System.Windows.Forms.TextBox();
+            this.textBoxDownloadPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(336, 64);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(75, 23);
+            this.buttonDownload.TabIndex = 0;
+            this.buttonDownload.Text = "Download";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(417, 64);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // progressBarDownload
+            // 
+            this.progressBarDownload.Location = new System.Drawing.Point(16, 123);
+            this.progressBarDownload.Name = "progressBarDownload";
+            this.progressBarDownload.Size = new System.Drawing.Size(480, 23);
+            this.progressBarDownload.TabIndex = 2;
+            // 
+            // lableProgress
+            // 
+            this.lableProgress.AutoSize = true;
+            this.lableProgress.Location = new System.Drawing.Point(13, 95);
+            this.lableProgress.Name = "lableProgress";
+            this.lableProgress.Size = new System.Drawing.Size(98, 13);
+            this.lableProgress.TabIndex = 3;
+            this.lableProgress.Text = "Awaiting Download";
+            // 
+            // lableDownloadURL
+            // 
+            this.lableDownloadURL.AutoSize = true;
+            this.lableDownloadURL.Location = new System.Drawing.Point(13, 15);
+            this.lableDownloadURL.Name = "lableDownloadURL";
+            this.lableDownloadURL.Size = new System.Drawing.Size(80, 13);
+            this.lableDownloadURL.TabIndex = 4;
+            this.lableDownloadURL.Text = "Download URL";
+            // 
+            // lableDownloadPath
+            // 
+            this.lableDownloadPath.AutoSize = true;
+            this.lableDownloadPath.Location = new System.Drawing.Point(13, 41);
+            this.lableDownloadPath.Name = "lableDownloadPath";
+            this.lableDownloadPath.Size = new System.Drawing.Size(80, 13);
+            this.lableDownloadPath.TabIndex = 10;
+            this.lableDownloadPath.Text = "Download Path";
+            // 
+            // textBoxDownloadURL
+            // 
+            this.textBoxDownloadURL.Location = new System.Drawing.Point(99, 12);
+            this.textBoxDownloadURL.Name = "textBoxDownloadURL";
+            this.textBoxDownloadURL.Size = new System.Drawing.Size(393, 20);
+            this.textBoxDownloadURL.TabIndex = 11;
+            // 
+            // textBoxDownloadPath
+            // 
+            this.textBoxDownloadPath.Location = new System.Drawing.Point(99, 38);
+            this.textBoxDownloadPath.Name = "textBoxDownloadPath";
+            this.textBoxDownloadPath.Size = new System.Drawing.Size(393, 20);
+            this.textBoxDownloadPath.TabIndex = 12;
+            // 
+            // DownloadManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 182);
+            this.Controls.Add(this.textBoxDownloadPath);
+            this.Controls.Add(this.textBoxDownloadURL);
+            this.Controls.Add(this.lableDownloadPath);
+            this.Controls.Add(this.lableDownloadURL);
+            this.Controls.Add(this.lableProgress);
+            this.Controls.Add(this.progressBarDownload);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonDownload);
+            this.Name = "DownloadManagerForm";
+            this.Text = "Download Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.ProgressBar progressBarDownload;
+        private System.Windows.Forms.Label lableProgress;
+        private System.Windows.Forms.Label lableDownloadURL;
+        private System.Windows.Forms.Label lableDownloadPath;
+        private System.Windows.Forms.TextBox textBoxDownloadURL;
+        private System.Windows.Forms.TextBox textBoxDownloadPath;
+    }
+}
+
---
+++ DownloadManager/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadManager
+{
+    public partial class DownloadManagerForm : Form
+    {
+        private Thread threadDownload;
+        private Stream streamResponse;
+        private Stream streamLocal;
+        private HttpWebRequest webRequest;
+        private HttpWebResponse webResponse;
+        private static int persentProgress;
+        private delegate void UpdateProgressCallback(int bytesRead, int totalBytes);
+
+        public DownloadManagerForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void UpdateProgress(int bytesRead, int totalBytes)
+        {
+            persentProgress = Convert.ToInt32((bytesRead * 100) / totalBytes);
+            progressBarDownload.Value = persentProgress;
+            lableProgress.Text = $"Download {bytesRead} out of {totalBytes} - {persentProgress}%";
+        }
+
+        private void Download()
+        {
+            using (WebClient webClientDownload = new WebClient())
+            {
+                try
+                {
+                    webRequest = (HttpWebRequest)WebRequest.Create(textBoxDownloadPath.Text);
+                    webRequest.Credentials = CredentialCache.DefaultCredentials;
+                    webResponse = (HttpWebResponse)webRequest.GetResponse();
+                    int fileSize = Int32.Parse(webResponse.ContentLength.ToString());
+
+                    streamResponse = webClientDownload.OpenRead(textBoxDownloadURL.Text);
+                    streamLocal = new FileStream(textBoxDownloadPath.Text, FileMode.Create, FileAccess.Write, FileShare.None);
+                    int bytesSize = 0;
+                    byte[] downloadBuffer = new byte[2048];
+
+                    while ((bytesSize = streamResponse.Read(downloadBuffer, 0, downloadBuffer.Length)) > 0)
+                    {
+                        streamLocal.Write(downloadBuffer, 0, bytesSize);
+                        Invoke(new UpdateProgressCallback(UpdateProgress), new object[] { streamLocal.Length, fileSize });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Some error: \n{ex}");
+                }
+                finally
+                {
+                    streamResponse.Close();
+                    streamLocal.Close();
+                }
+            }
+        }
+
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            lableProgress.Text = "Download Starting";
+            threadDownload = new Thread(Download);
+            threadDownload.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            webResponse.Close();
+            streamResponse.Close();
+            streamLocal.Close();
+            threadDownload.Abort();
+            progressBarDownload.Value = 0;
+            lableProgress.Text = "Download Stoppet";
+        }
+    }
+}
